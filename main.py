8093ef220505b3638a4f2c92e59cc5cdc7a59832
IDGS-803-21000011 <paulovaladez813@gmail.com>
2024-01-26 18:00:57 -0600

    PEVG 26/01/2024 Practica cinepolis

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("formulario.html")
+    return render_template("layout.html")
 
 @app.route("/formulario")
 def formulario():
     return render_template("formulario.html")
+
+@app.route("/cine")
+def cine():
+    return render_template("vistaCine.html")
 
 @app.route("/resultado", methods=["GET", "POST"])
 def resultado():
@@ -26,6 +30,43 @@ def resultado():
         elif operacion == "division":
             resultado = int(num1) / int(num2)
         return "<h1>La multiplicacion es: {}</h1>".format(str(resultado))
+    
+@app.route("/resultadoCine", methods=["POST"])
+def resultadoCine():
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        boletos = request.form.get("boletos")
+        tarjeta = request.form.get("tarjeta")
+        compradores = request.form.get("compradores")
+        resultado_multiplicacion = 0
+        boletos_permitidos = int(compradores) * 7
+
+        if not nombre or not boletos.isdigit():
+            return "Error: Datos de entrada no válidos."
+
+        boletos_enteros = int(boletos)
+        if boletos_enteros > boletos_permitidos:
+            resultado_multiplicacion = 0
+        else: 
+            if boletos_enteros > 5:
+                if tarjeta == "si":
+                    resultado_multiplicacion = (boletos_enteros * 12) - ((boletos_enteros * 12) * 0.15)
+                    resultado_multiplicacion = resultado_multiplicacion - (resultado_multiplicacion * 0.10) 
+                else:
+                    resultado_multiplicacion = (boletos_enteros * 12) - ((boletos_enteros * 12) * 0.15)
+            elif boletos_enteros == 3 or boletos_enteros == 4 or boletos_enteros == 5:
+                if tarjeta == "si":
+                    resultado_multiplicacion = (boletos_enteros * 12) - ((boletos_enteros * 12) * 0.10)
+                    resultado_multiplicacion = resultado_multiplicacion - (resultado_multiplicacion * 0.10)
+                else:
+                    resultado_multiplicacion = (boletos_enteros * 12) - ((boletos_enteros * 12) * 0.10)
+            else:
+                if tarjeta == "si":
+                    resultado_multiplicacion = (boletos_enteros * 12) * .10
+                else: 
+                    resultado_multiplicacion = boletos_enteros * 12
+
+        return render_template("vistaCine.html", resultado=resultado_multiplicacion)
 
 if __name__ == "__main__":
     app.run(debug=True)
